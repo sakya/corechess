da2be7814ca7a845a180a2d44786f0b8f612d5dc
Paolo Iommarini <paolo.iommarini@live.com>
2022-02-15 15:33:46 +0100

    Fixed publish.bat

--- publish.bat
+++ publish.bat
@@ -1,4 +1,9 @@
 rd /s /q .\dist\win-x64
+rd /s /q .\ChessLib\bin
+rd /s /q .\ChessLib\obj
+rd /s /q .\CoreChess\bin
+rd /s /q .\CoreChess\obj
+
 dotnet clean CoreChess.sln -c Release
 dotnet publish CoreChess.sln -c Release --runtime win-x64 -p:PublishReadyToRun=true --self-contained --output .\dist\win-x64
 copy CoreChess\Lib\Bass\bass.dll dist\win-x64\
