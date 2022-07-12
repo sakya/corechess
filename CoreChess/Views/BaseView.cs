90102a03d8a4131ea23cd46696f354400e3f2557
Paolo Iommarini <paolo.iommarini@xenialab.it>
2022-07-12 11:48:00 +0200

    Added option to restore window size and position

--- CoreChess/Views/BaseView.cs
+++ CoreChess/Views/BaseView.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Platform;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -45,6 +46,46 @@ namespace CoreChess.Views
                 this.Title = $"CoreChess - {title}";
         }
 
+        protected void SaveWindowSizeAndPosition()
+        {
+            // Save window size and position
+            var screen = this.Screens.ScreenFromVisual(this);
+            var ws = new WindowSize()
+            {
+                State = this.WindowState,
+
+                Width = this.Bounds.Width,
+                Height = this.Bounds.Height,
+
+                Y = this.Position.Y,
+                X = this.Position.X
+            };
+            ws.Save(Path.Join(App.LocalPath, $"ws{this.GetType().Name}.json"));
+        } // SaveWindowSizeAndPosition
+
+        protected void RestoreWindowSizeAndPosition()
+        {
+            WindowSize ws = null;
+            try {
+                ws = WindowSize.Load(Path.Join(App.LocalPath, $"ws{this.GetType().Name}.json"));
+            } catch {
+                return;
+            }
+
+            var screen = this.Screens.ScreenFromVisual(this);
+            if (ws.State == WindowState.Maximized || 
+                ws.Width <= screen.Bounds.Width && ws.Height <= screen.Bounds.Height && ws.X <= screen.Bounds.Width && ws.Y <= screen.Bounds.Height) {
+                this.WindowStartupLocation = WindowStartupLocation.Manual;
+                this.WindowState = ws.State;
+                if (ws.State == WindowState.Normal) {
+                    this.Width = ws.Width;
+                    this.Height = ws.Height;
+
+                    this.Position = new PixelPoint(ws.X, ws.Y);
+                }
+            }
+        } // RestoreWindowSizeAndPosition
+
         private async void CenterWindow()
         {
             if (this.WindowStartupLocation == WindowStartupLocation.Manual)
@@ -58,18 +99,14 @@ namespace CoreChess.Views
             }
             PixelRect rect = new PixelRect(PixelPoint.Origin,
                 PixelSize.FromSize(ClientSize, scale));
-            if(WindowStartupLocation == WindowStartupLocation.CenterScreen) {
+            if (WindowStartupLocation == WindowStartupLocation.CenterScreen) {
                 Screen screen = Screens.ScreenFromPoint(powner?.Position ?? Position);
-                if(screen == null) {
+                if(screen == null)
                     return;
-                }
                 Position = screen.WorkingArea.CenterRect(rect).Position;
-            }
-            else {
-                if(powner == null ||
-                    WindowStartupLocation != WindowStartupLocation.CenterOwner) {
+            } else {
+                if(powner == null || WindowStartupLocation != WindowStartupLocation.CenterOwner) 
                     return;
-                }
                 Position = new PixelRect(powner.Position,
                     PixelSize.FromSize(powner.ClientSize, scale)).CenterRect(rect).Position;
             }
