cef927306bac493cd92e2d436fd5f80e5ff6b445
Paolo Iommarini <paolo.iommarini@xenialab.it>
2023-08-01 10:23:03 +0200

    Fixed WindowSize

--- CoreChess/WindowSize.cs
+++ CoreChess/WindowSize.cs
@@ -1,20 +1,11 @@
 ﻿using Avalonia.Controls;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CoreChess
 {
     public class WindowSize
     {
-        public WindowSize()
-        {
-        }
-
         public WindowState State { get; set; }
 
         public double Width { get; set; }
@@ -25,16 +16,14 @@ namespace CoreChess
 
         public void Save(string path)
         {
-            using (StreamWriter sw = new StreamWriter(path)) {
-                sw.Write(JsonConvert.SerializeObject(this, Formatting.Indented));
-            }
+            using var sw = new StreamWriter(path);
+            sw.Write(JsonConvert.SerializeObject(this, Formatting.Indented));
         } // Save
 
         public static WindowSize Load(string path)
         {
-            using (StreamReader sr = new StreamReader(path)) {
-                return JsonConvert.DeserializeObject<WindowSize>(sr.ReadToEnd());
-            }
+            using var sr = new StreamReader(path);
+            return JsonConvert.DeserializeObject<WindowSize>(sr.ReadToEnd());
         } // Load
     }
 }
